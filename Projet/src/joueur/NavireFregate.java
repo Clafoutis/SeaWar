b09b2a0b601933529ff04bd178d17868190f53c5
lukijo <mickae56@yahoo.fr>
2017-12-13 18:19:59 +0100

    yeah

--- Projet/src/joueur/NavireFregate.java
+++ Projet/src/joueur/NavireFregate.java
@@ -8,6 +8,12 @@ public class NavireFregate extends Navire {
 
 	public NavireFregate(Point _origine) throws SlickException {
 		super(_origine);
+		setNbDeplacements(7);
+		setPv(50);
+		setDmgCannonPrincipal(30);
+		setDmgCanonSecondaire(10);
+		setNbTourRechargeCanonPrincipal(1);
+		setNbTourRechargeCanonSecondaire(0);
 		// TODO Auto-generated constructor stub
 	}
 
