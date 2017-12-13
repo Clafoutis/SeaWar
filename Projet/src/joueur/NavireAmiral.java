48b6184bcd3e130964eb2c98b3f510412713fa9b
lukijo <mickae56@yahoo.fr>
2017-12-13 18:19:39 +0100

    yeah

--- Projet/src/joueur/NavireAmiral.java
+++ Projet/src/joueur/NavireAmiral.java
@@ -8,6 +8,12 @@ public class NavireAmiral extends Navire {
 
 	public NavireAmiral(Point _origine) throws SlickException {
 		super(_origine);
+		setNbDeplacements(3);
+       	 	setPv(100);
+        	setDmgCannonPrincipal(50);
+        	setDmgCanonSecondaire(30);
+        	setNbTourRechargeCanonPrincipal(3);
+        	setNbTourRechargeCanonSecondaire(1);
 		// TODO Auto-generated constructor stub
 	}
 
