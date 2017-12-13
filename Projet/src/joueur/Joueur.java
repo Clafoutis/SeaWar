640eaaf2e8067b5b1b792e5265389885fb836063
lukijo <mickae56@yahoo.fr>
2017-12-13 18:14:03 +0100

    Update joueur

--- Projet/src/joueur/Joueur.java
+++ Projet/src/joueur/Joueur.java
@@ -1,5 +1,30 @@
 package joueur;
 
-public class Joueur {
+import java.awt.*;
+import java.util.ArrayList;
 
+public class Joueur {
+    private String nom;
+    private Color couleur;
+    private ArrayList<Navire> navires;
+
+    public Joueur(String nom, Color couleur){
+        this.nom = nom;
+        this.couleur = couleur;
+        this.navires = new ArrayList<Navire>();
+        this.navires.add(new NavireAmiral());
+        this.navires.add(new NavireFregate());
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public Color getCouleur() {
+        return couleur;
+    }
+
+    public Navire getNavire(int id) {
+        return navires.get(id);
+    }
 }
