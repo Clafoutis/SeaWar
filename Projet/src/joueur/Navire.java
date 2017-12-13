31d735ea8068caf18073afbb810f56ee3a09d782
Magnism <maggnism@gmail.com>
2017-12-13 18:10:36 +0100

    update navire

--- Projet/src/joueur/Navire.java
+++ Projet/src/joueur/Navire.java
@@ -1,7 +1,44 @@
 package joueur;
 
+import java.awt.Point;
+
+import map.Map;
+
 import org.newdawn.slick.Animation;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
+
+import utility.FileUtility;
 
 public class Navire extends Animation {
-
+	public static final String FICHIER_SPRITE_SHEET_NAVIRE = "boat.png";
+	public static final int LONGUEUR_COTE_TUILE = 64;
+	
+	private SpriteSheet spriteSheet;
+	
+	private Point origine;
+	private Point position = new Point();
+	
+	public Navire(Point _origine) throws SlickException {
+		origine = _origine;
+		spriteSheet = new SpriteSheet(FileUtility.DOSSIER_SPRITE + FICHIER_SPRITE_SHEET_NAVIRE, LONGUEUR_COTE_TUILE, LONGUEUR_COTE_TUILE);
+		addFrame(spriteSheet.getSprite(0, 0), 100);
+	}
+	
+	public Point getPosition() {
+		return (Point)position.clone();
+	}
+	
+	public void setPosition(Point _position) {
+		position = _position;
+	}
+	
+	public void allerHaut() {
+		Map.getInstance().allerHaut(this);
+	}
+	
+	@Override
+	public void draw() {
+		this.draw(origine.x + position.x, origine.y + position.y);
+	}
 }
--- Projet/src/joueur/NavireFregate.java
+++ Projet/src/joueur/NavireFregate.java
@@ -1,5 +1,14 @@
 package joueur;
 
+import java.awt.Point;
+
+import org.newdawn.slick.SlickException;
+
 public class NavireFregate extends Navire {
+
+	public NavireFregate(Point _origine) throws SlickException {
+		super(_origine);
+		// TODO Auto-generated constructor stub
+	}
 
 }
