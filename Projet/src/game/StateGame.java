613c70ec8526553c237949bbb262f94122250fe4
Magnism <maggnism@gmail.com>
2017-12-20 15:38:48 +0100

    Update Collisions

--- Projet/src/game/StateGame.java
+++ Projet/src/game/StateGame.java
@@ -8,6 +8,8 @@ import org.newdawn.slick.opengl.renderer.Renderer;
 import org.newdawn.slick.state.StateBasedGame;
 import utility.Music;
 
+import map.Map;
+
 public class StateGame extends StateBasedGame {
 	public StateGame() {
 		super("Sea War");
