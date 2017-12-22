61c929fcc442925eba67b1a86ab4732d7485d8ff
lukijo <mickae56@yahoo.fr>
2017-12-22 15:47:50 +0100

    ajout nouveau sprite et direction

--- Projet/src/joueur/Navire.java
+++ Projet/src/joueur/Navire.java
@@ -11,59 +11,78 @@ import org.newdawn.slick.SpriteSheet;
 
 import utility.FileUtility;
 
-public class Navire extends Animation {
-	public static final String FICHIER_SPRITE_SHEET_NAVIRE = "boat.png";
-	public static final int LONGUEUR_COTE_TUILE = 64;
-	private int pv, nbDeplacements, dmgCannonPrincipal, dmgCanonSecondaire,
+public class Navire {
+    public static final String FICHIER_SPRITE_SHEET_NAVIRE = "boat2.png";
+    public static final int LONGUEUR_COTE_TUILE = 64;
+    private int pv, nbDeplacements, dmgCannonPrincipal, dmgCanonSecondaire,
             nbTourRechargeCanonPrincipal, nbTourRechargeCanonSecondaire;
-	
-	private SpriteSheet spriteSheet;
-	
-	private Point position = new Point();
-	
-	public Navire() throws SlickException {
-		spriteSheet = new SpriteSheet(FileUtility.DOSSIER_SPRITE + FICHIER_SPRITE_SHEET_NAVIRE, LONGUEUR_COTE_TUILE, LONGUEUR_COTE_TUILE);
-		addFrame(spriteSheet.getSprite(0, 0), 100);
-	}
-	
-	public void setPv(int pv) {
-		this.pv = pv;
-	}
+    private int direction = 0;
+    private SpriteSheet spriteSheet;
+    private Animation[] animations = new Animation[6];
+    private Point position = new Point();
 
-	public void setNbDeplacements(int nbDeplacements) {
-		this.nbDeplacements = nbDeplacements;
-	}
+    public Navire() throws SlickException {
+        spriteSheet = new SpriteSheet(FileUtility.DOSSIER_SPRITE + FICHIER_SPRITE_SHEET_NAVIRE, LONGUEUR_COTE_TUILE, LONGUEUR_COTE_TUILE);
+        this.animations[0] = loadAnimation(spriteSheet, 1, 1);
+        this.animations[1] = loadAnimation(spriteSheet, 0, 1);
+        this.animations[2] = loadAnimation(spriteSheet, 1, 0);
+        this.animations[3] = loadAnimation(spriteSheet, 0, 0);
+        this.animations[4] = loadAnimation(spriteSheet, 2, 1);
+        this.animations[5] = loadAnimation(spriteSheet, 2, 0);
+        //addFrame(spriteSheet.getSprite(0, 0), 100);
+    }
 
-	public void setDmgCannonPrincipal(int dmgCannonPrincipal) {
-		this.dmgCannonPrincipal = dmgCannonPrincipal;
-	}
+    private Animation loadAnimation(SpriteSheet spriteSheet, int x, int y) {
+        Animation animation = new Animation();
+        animation.addFrame(spriteSheet.getSprite(x, y), 100);
+        return animation;
+    }
 
-	public void setDmgCanonSecondaire(int dmgCanonSecondaire) {
-		this.dmgCanonSecondaire = dmgCanonSecondaire;
- 	}
+    public void setPv(int pv) {
+        this.pv = pv;
+    }
 
-	public void setNbTourRechargeCanonPrincipal(int nbTourRechargeCanonPrincipal) {
-		this.nbTourRechargeCanonPrincipal = nbTourRechargeCanonPrincipal;
-	}
+    public void setNbDeplacements(int nbDeplacements) {
+        this.nbDeplacements = nbDeplacements;
+    }
 
-	public void setNbTourRechargeCanonSecondaire(int nbTourRechargeCanonSecondaire) {
-		this.nbTourRechargeCanonSecondaire = nbTourRechargeCanonSecondaire;
-	}
-	
-	public Point getPosition() {
-		return (Point)position.clone();
-	}
-	
-	public void setPosition(Point _position) {
-		position = _position;
-	}
-	
-	public void deplacer(Direction direction) {
-		Map.getInstance().deplacer(this, direction);
-	}
-	
-	@Override
-	public void draw() {
-		this.draw(Map.getInstance().getPosition().x + position.x, Map.getInstance().getPosition().y + position.y);
-	}
+    public void setDmgCannonPrincipal(int dmgCannonPrincipal) {
+        this.dmgCannonPrincipal = dmgCannonPrincipal;
+    }
+
+    public void setDmgCanonSecondaire(int dmgCanonSecondaire) {
+        this.dmgCanonSecondaire = dmgCanonSecondaire;
+    }
+
+    public void setNbTourRechargeCanonPrincipal(int nbTourRechargeCanonPrincipal) {
+        this.nbTourRechargeCanonPrincipal = nbTourRechargeCanonPrincipal;
+    }
+
+    public void setNbTourRechargeCanonSecondaire(int nbTourRechargeCanonSecondaire) {
+        this.nbTourRechargeCanonSecondaire = nbTourRechargeCanonSecondaire;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public Point getPosition() {
+        return (Point)position.clone();
+    }
+
+    public void setPosition(Point _position) {
+        position = _position;
+    }
+
+    public void deplacer(Direction direction) {
+        Map.getInstance().deplacer(this, direction);
+    }
+
+    public void draw() {
+        animations[direction].draw(Map.getInstance().getPosition().x + position.x, Map.getInstance().getPosition().y + position.y);
+    }
 }
