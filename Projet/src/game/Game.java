1fe2959f88019d31763fc3e07e8e129c4f836935
lukijo <mickae56@yahoo.fr>
2018-01-16 17:37:40 +0100

    Début partie

--- Projet/src/game/Game.java
+++ Projet/src/game/Game.java
@@ -1,15 +1,11 @@
 package game;
 
 import java.awt.Color;
-import java.awt.Image;
 import java.awt.Point;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
-import org.newdawn.slick.Renderable;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.state.BasicGameState;
@@ -24,36 +20,28 @@ public class Game extends BasicGameState {
     public static final int ID = 2;
     private GameContainer container;
     private StateBasedGame game;
-
     private Animation background;
+    private ArrayList<Point> bufferClick; // Changer le type
+    private Point position = new Point();
+    // Déroulement partie
     private final int NB_JOUEURS = 2;
     private Joueur[] joueurs = new Joueur[NB_JOUEURS];
     private Joueur joueurCourant;
-    private ArrayList<Point> bufferClick; // Changer le type
-    private Point position = new Point();
-    //private Animation AmiralPirate;
-    //private SpriteSheet spriteSheetdeNavire;
+
 
     @Override
     public void init(GameContainer _container, StateBasedGame _game) throws SlickException {
         //spriteSheetdeNavire = new SpriteSheet("resources/images/AmiralPirate.png",200 , 250);
         //AmiralPirate=new Animation(spriteSheetdeNavire,8000);
         //this.AmiralPirate = ImageIO.read(getClass().getResource("resources/images/AmiralPirate.png"));
-
-        SpriteSheet spriteSheet = new SpriteSheet("resources/images/seaAnimation.png", 1080, 810);
         this.game = _game;
         this.container = _container;
-        bufferClick = new ArrayList<Point>();
-        Map.getInstance().load("test.txt");
-        Map.getInstance().centrerDansFenetre(container);
-        joueurs[0] = new Joueur("Joueur 1", Color.RED, 1);
-        joueurs[1] = new Joueur("Joueur 2", Color.BLUE, 2);
-        joueurCourant = joueurs[0];
-
+        SpriteSheet spriteSheet = new SpriteSheet("resources/images/seaAnimation.png", 1080, 810);
         this.background = new Animation();
         for (int x = 0; x < 2; x++) {
             background.addFrame(spriteSheet.getSprite(x, 0), 1200);
         }
+        bufferClick = new ArrayList<>();
     }
 
     @Override
@@ -62,9 +50,9 @@ public class Game extends BasicGameState {
         if(joueurCourant.deplacementEnCours()){
             joueurCourant.getNavireCourant().animationDeplacement();
         }
-        
+
         if(!bufferClick.isEmpty() && !joueurCourant.getNavireCourant().isDeplacementEnCours()){
-        	executeClick();
+            executeClick();
         }
     }
 
@@ -74,16 +62,22 @@ public class Game extends BasicGameState {
         Map.getInstance().draw();
         graphics.drawString( "Joueur 1 :", 0, 30);
         graphics.drawString( "Points de vie Amiral :"+joueurs[0].getNavire(0).getPv(), 0, 50);
-    	graphics.drawString( "Points de vie Frégate :"+joueurs[0].getNavire(1).getPv(), 0, 70);
-    	graphics.drawString( "nbDeplacements Amiral :"+joueurs[0].getNavire(0).getNbDeplacements(), 0, 90);
-    	graphics.drawString( "nbDeplacements Frégate :"+joueurs[0].getNavire(1).getNbDeplacements(), 0, 110);
-    	graphics.drawString( "Joueur 2 :", 800, 30);
-    	 graphics.drawString( "Points de vie Amiral :"+joueurs[1].getNavire(0).getPv(), 800, 50);
-     	graphics.drawString( "Points de vie Frégate :"+joueurs[1].getNavire(1).getPv(), 800, 70);
-     	graphics.drawString( "nbDeplacements Amiral :"+joueurs[1].getNavire(0).getNbDeplacements(), 800, 90);
-     	graphics.drawString( "nbDeplacements Frégate :"+joueurs[1].getNavire(1).getNbDeplacements(), 800, 110);
-        //AmiralPirate.draw(56,35);
-        //joueurCourant.getNavireCourant().draw();
+        graphics.drawString( "Points de vie Frégate :"+joueurs[0].getNavire(1).getPv(), 0, 70);
+        graphics.drawString( "nbDeplacements Amiral :"+joueurs[0].getNavire(0).getNbDeplacementsRestants(), 0, 90);
+        graphics.drawString( "nbDeplacements Frégate :"+joueurs[0].getNavire(1).getNbDeplacementsRestants(), 0, 110);
+        graphics.drawString( "Joueur 2 :", 800, 30);
+        graphics.drawString( "Points de vie Amiral :"+joueurs[1].getNavire(0).getPv(), 800, 50);
+        graphics.drawString( "Points de vie Frégate :"+joueurs[1].getNavire(1).getPv(), 800, 70);
+        graphics.drawString( "nbDeplacements Amiral :"+joueurs[1].getNavire(0).getNbDeplacementsRestants(), 800, 90);
+        graphics.drawString( "nbDeplacements Frégate :"+joueurs[1].getNavire(1).getNbDeplacementsRestants(), 800, 110);
+    }
+
+    public void newGame() throws SlickException {
+        Map.getInstance().load("test.txt");
+        Map.getInstance().centrerDansFenetre(container);
+        joueurs[0] = new Joueur("Joueur 1", Color.RED, 1);
+        joueurs[1] = new Joueur("Joueur 2", Color.BLUE, 2);
+        joueurCourant = joueurs[0];
     }
 
     @Override
@@ -141,17 +135,14 @@ public class Game extends BasicGameState {
 
     public void mouseClicked(int button, int x, int y, int clickCount){
         Point coordMaillage = new Point(x - (int) Map.getInstance().getPosition().getX(), y - (int) Map.getInstance().getPosition().getY());
-        Point coordPosTab = Map.getInstance().coordMaillageToTab(position);
-        
-        
-        if(!bufferClick.contains(Map.getInstance().coordMaillageToTab(coordMaillage))){ // test provisoire
-        	bufferClick.add(Map.getInstance().coordMaillageToTab(coordMaillage));
+        if(!bufferClick.contains(Map.getInstance().coordMaillageToTab(coordMaillage))){
+            bufferClick.add(Map.getInstance().coordMaillageToTab(coordMaillage));
         }
     }
-    
+
     public void executeClick(){
-    	joueurCourant.getNavireCourant().tryAccess(bufferClick.get(0));
-    	bufferClick.remove(0); // On supprime l'action enregistrée dans tous les cas. Si la méthode try access s'est bien déroulée et si elle ne s'est pas bien déroulée. Il ne faut pas encombrer le buffer avec des actions invalides.
+        joueurCourant.getNavireCourant().tryAccess(bufferClick.get(0));
+        bufferClick.remove(0); // On supprime l'action enregistrée dans tous les cas. Si la méthode try access s'est bien déroulée et si elle ne s'est pas bien déroulée. Il ne faut pas encombrer le buffer avec des actions invalides.
     }
     @Override
     public int getID() {
