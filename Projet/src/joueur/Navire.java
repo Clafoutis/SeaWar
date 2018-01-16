ef0d32041555a9fe6489e0959c2dc0e73b006b90
Clafoutis <arthur.roussel38@gmail.com>
2018-01-16 17:04:28 +0100

    Add files via upload
    concept POO vérifié

--- Projet/src/joueur/Navire.java
+++ Projet/src/joueur/Navire.java
@@ -14,9 +14,12 @@ import utility.FileUtility;
 import static java.lang.Thread.sleep;
 
 public class Navire {
+    public static final String FICHIER_SPRITE_SHEET_NAVIRE = "boat2.png";
+    public static final int LONGUEUR_COTE_TUILE = 64;
     private int pv, nbDeplacements, dmgCannonPrincipal, dmgCanonSecondaire,
             nbTourRechargeCanonPrincipal, nbTourRechargeCanonSecondaire;
     private int direction;
+    private SpriteSheet spriteSheet;
     private Animation[] animations = new Animation[6];
     private Point position = new Point();
     // Pour les déplacements animés
@@ -25,17 +28,23 @@ public class Navire {
     private int nbDeplacementsRestants;
     private Point destination;
 
-    public Navire(int direction, SpriteSheet spriteSheet) throws SlickException {
+    public Navire(int direction) throws SlickException {
         this.direction = direction;
         deplacementEnCours = false;
-        this.animations[0] = loadAnimation(spriteSheet, 0, 0);
+        spriteSheet = new SpriteSheet(FileUtility.DOSSIER_SPRITE + FICHIER_SPRITE_SHEET_NAVIRE, LONGUEUR_COTE_TUILE, LONGUEUR_COTE_TUILE);
+        this.animations[0] = loadAnimation(spriteSheet, 1, 1);
         this.animations[1] = loadAnimation(spriteSheet, 0, 1);
-        this.animations[2] = loadAnimation(spriteSheet, 0, 2);
-        this.animations[3] = loadAnimation(spriteSheet, 0, 3);
-        this.animations[4] = loadAnimation(spriteSheet, 0, 4);
-        this.animations[5] = loadAnimation(spriteSheet, 0, 5);
+        this.animations[2] = loadAnimation(spriteSheet, 1, 0);
+        this.animations[3] = loadAnimation(spriteSheet, 0, 0);
+        this.animations[4] = loadAnimation(spriteSheet, 2, 1);
+        this.animations[5] = loadAnimation(spriteSheet, 2, 0);
+        //addFrame(spriteSheet.getSprite(0, 0), 100);
     }
 
+    public int getPv(){
+    	return this.pv;
+    }
+    
     private Animation loadAnimation(SpriteSheet spriteSheet, int x, int y) {
         Animation animation = new Animation();
         animation.addFrame(spriteSheet.getSprite(x, y), 100);
@@ -44,6 +53,9 @@ public class Navire {
 
     public void setPv(int pv) {
         this.pv = pv;
+    }
+    public int getNbDeplacements(){
+    	return this.nbDeplacements;
     }
 
     public void setNbDeplacements(int nbDeplacements) {
@@ -87,56 +99,58 @@ public class Navire {
     }
 
     public void tryAccess(Point coordCibleTab) {
-        Point coordPosTab = Map.getInstance().coordMaillageToTab(position);
-        int difX = (int) (coordCibleTab.getX()-coordPosTab.getX());
-        int difY = (int) (coordCibleTab.getY()-coordPosTab.getY());
-        int sensPremierDenivele = Map.getInstance().getSensPremierDenivele();
-        boolean xPaire = (coordPosTab.getX() % 2) == 0;
-        boolean deniveleInverse = sensPremierDenivele == 1;
-        boolean wtfIsThisShit = xPaire == deniveleInverse;
-        boolean ohMyGodness = xPaire != deniveleInverse;
-        switch(difX){
-            case -1:
-                switch(difY){
-                    case -1:
-                        if(wtfIsThisShit) deplacer(Direction.HAUT_GAUCHE);
-                        break;
-                    case 0:
-                        if(ohMyGodness) deplacer(Direction.HAUT_GAUCHE);
-                        else deplacer(Direction.BAS_GAUCHE);
-                        break;
-                    case 1:
-                        if(ohMyGodness) deplacer(Direction.BAS_GAUCHE);
-                        break;
-                }
-                break;
-            case 0:
-                switch(difY){
-                    case -1:
-                        deplacer(Direction.HAUT);
-                        break;
-                    case 1:
-                        deplacer(Direction.BAS);
-                        break;
-                }
-                break;
-            case 1:
-                switch(difY){
-                    case -1:
-                        if(wtfIsThisShit) deplacer(Direction.HAUT_DROITE);
-                        break;
-                    case 0:
-                        if(ohMyGodness) deplacer(Direction.HAUT_DROITE);
-                        else deplacer(Direction.BAS_DROITE);
-                        break;
-                    case 1:
-                        if(ohMyGodness) deplacer(Direction.BAS_DROITE);
-                        break;
-                }
-                break;
+    	if(!deplacementEnCours){
+	        Point coordPosTab = Map.getInstance().coordMaillageToTab(position);
+	        int difX = (int) (coordCibleTab.getX()-coordPosTab.getX());
+	        int difY = (int) (coordCibleTab.getY()-coordPosTab.getY());
+	        int sensPremierDenivele = Map.getInstance().getSensPremierDenivele();
+	        boolean xPaire = (coordPosTab.getX() % 2) == 0;
+	        boolean deniveleInverse = sensPremierDenivele == 1;
+	        boolean wtfIsThisShit = xPaire == deniveleInverse;
+	        boolean ohMyGodness = xPaire != deniveleInverse;
+	        switch(difX){
+	            case -1:
+	                switch(difY){
+	                    case -1:
+	                        if(wtfIsThisShit) deplacer(Direction.HAUT_GAUCHE);
+	                        break;
+	                    case 0:
+	                        if(ohMyGodness) deplacer(Direction.HAUT_GAUCHE);
+	                        else deplacer(Direction.BAS_GAUCHE);
+	                        break;
+	                    case 1:
+	                        if(ohMyGodness) deplacer(Direction.BAS_GAUCHE);
+	                        break;
+	                }
+	                break;
+	            case 0:
+	                switch(difY){
+	                    case -1:
+	                        deplacer(Direction.HAUT);
+	                        break;
+	                    case 1:
+	                        deplacer(Direction.BAS);
+	                        break;
+	                }
+	                break;
+	            case 1:
+	                switch(difY){
+	                    case -1:
+	                        if(wtfIsThisShit) deplacer(Direction.HAUT_DROITE);
+	                        break;
+	                    case 0:
+	                        if(ohMyGodness) deplacer(Direction.HAUT_DROITE);
+	                        else deplacer(Direction.BAS_DROITE);
+	                        break;
+	                    case 1:
+	                        if(ohMyGodness) deplacer(Direction.BAS_DROITE);
+	                        break;
+	                }
+	                break;
+	        }
         }
     }
-
+    
     public void deplacer(Direction direction) {
         Map.getInstance().deplacer(this, direction);
     }
@@ -146,12 +160,12 @@ public class Navire {
     }
 
     public void initialiserDeplacement(Point position){
-        deltaX = (position.getX() - this.position.getX())/60;
-        deltaY = (position.getY() - this.position.getY())/60;
+        deltaX = (position.getX() - this.position.getX())/30;
+        deltaY = (position.getY() - this.position.getY())/30;
         tempX = this.position.getX();
         tempY = this.position.getY();
         destination = position;
-        nbDeplacementsRestants = 60;
+        nbDeplacementsRestants = 30;
         deplacementEnCours = true;
     }
 
