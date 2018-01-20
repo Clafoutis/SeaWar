f747d78cc2e12f6c3d97fdcb2c843257d6400adb
Magnism <maggnism@gmail.com>
2018-01-20 16:17:21 +0100

    Ajout d'une fonction getNbNavires()

--- Projet/src/joueur/Joueur.java
+++ Projet/src/joueur/Joueur.java
@@ -67,7 +67,6 @@ public class Joueur {
     }
 
     public void newTurn(){
-	    Map.getInstance().verifierPriseDePhare(this);
 	    navires.get(0).setNbDeplacementsRestants(navires.get(0).getNbDeplacements());
 	    navires.get(1).setNbDeplacementsRestants(navires.get(1).getNbDeplacements());
     }
