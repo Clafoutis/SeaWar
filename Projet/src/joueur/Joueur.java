5e213ceb86afa5b6c056614e735eeecae3e54819
lukijo <mickae56@yahoo.fr>
2018-02-08 23:46:57 +0100

    javadoc teur

--- Projet/src/joueur/Joueur.java
+++ Projet/src/joueur/Joueur.java
@@ -5,6 +5,9 @@ import map.Map;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Color;
 
+/**
+ * Element qui défini un joueur dans le jeu
+ */
 public class Joueur {
     private int id;
     private String nom;
@@ -12,6 +15,13 @@ public class Joueur {
     private ArrayList<Navire> navires;
 	private Navire navireCourant;
 	
+	/**
+	 * créé un joueur dans le jeu
+	 * @param id représente l'id du joueur
+	 * @param nom représente le nom du joueur
+	 * @param couleur représente la couleur du joueur
+	 * @param numero représente le numéro du joueur
+	 */
 	public Joueur(int id, String nom, Color couleur, int numero) throws SlickException{
 		this.id = id;
         this.nom = nom;
@@ -45,44 +55,84 @@ public class Joueur {
         Map.getInstance().addNavire(navires.get(1), posTabXNavire2, posTabYNavire2);
     }
 
+	/**
+	 * Retourne l'id du joueur
+	 * @return l'id du joueur
+	 */
     public int getId() {
         return id;
     }
 
+	/**
+	 * Retourne le nom du joueur
+	 * @return le nom du joueur
+	 */
     public String getNom() {
         return nom;
     }
 
+	/**
+	 * Retourne la couleur du joueur
+	 * @return la couleur du joueur
+	 */
     public Color getCouleur() {
         return couleur;
     }
 
+	/**
+	 * Retourne un navire du joueur
+	 * @param id représente l'id du navire à sélectionner
+	 * @return le navire sélectionné
+	 */
     public Navire getNavire(int id) {
         return navires.get(id);
     }
 
+	/**
+	 * Retourne le nombre de navires du joueur
+	 * @return le nombre de navires du joueur
+	 */
     public int getNbNavires() {
     	return navires.size();
     }
 
+	/**
+	 * modifie le navire courant du joueur (navire sélectionné)
+	 * @param navireCourant représente le navire
+	 */
     public void setNavireCourant(Navire navireCourant) {
         this.navireCourant = navireCourant;
     }
 
+	/**
+	 * Retourne le navire courant du joueur
+	 * @return le navire courant du joueur
+	 */
     public Navire getNavireCourant() {
 		return navireCourant;
 	}
 
+	/**
+	 * Permet de savoir si le joueur a son navire courant en cours de déplacement
+	 * @return booléen permettant de savoir si le joueur a son navire courant en cours de déplacement
+	 */
 	public boolean deplacementEnCours(){
         return navireCourant.isDeplacementEnCours();
     }
 
+	/**
+	 * Retourne le nombre de bateaux en vie du joueur
+	 * @return le nombre de bateaux en vie du joueur
+	 */
     public int nbBateauEnVie() {
         int nb = 0;
         for (Navire navire : navires) if (!navire.isEtatDetruit()) nb++;
         return nb;
     }
 
+	/**
+	 * met à jour l'état du joueur lors d'un nouveau tour
+	 */
     public void newTurn(){
 	    navires.get(0).setNbDeplacementsRestants(navires.get(0).getNbDeplacements());
 	    navires.get(0).newTurn();
