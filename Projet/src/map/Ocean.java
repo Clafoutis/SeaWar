831d3d66e16eee44889913c103d45e6cf22e6b66
Magnism <maggnism@gmail.com>
2018-02-08 14:39:18 +0100

    Javadoc de toute les classes du package map

--- Projet/src/map/Ocean.java
+++ Projet/src/map/Ocean.java
@@ -4,11 +4,18 @@ import java.awt.Point;
 
 import org.newdawn.slick.SpriteSheet;
 
+/**
+ * Case spécifique représentant l'Océan (cf. classe Case pour plus d'informations)
+ */
 public class Ocean extends Case {
 	public static final int ID = 0;
 	private static final int NB_FRAMES_ANIMATION = 1;
 	private static final int DUREE_FRAME = 100;
 	
+	/**
+	 * Créée une case Océan.
+	 * @param position La position de la case (dans la fenêtre) relative à l'origine de la map.
+	 */
 	public Ocean(Point position) {
 		super(0, NB_FRAMES_ANIMATION, DUREE_FRAME, position);
 	}
--- Projet/src/map/Phare.java
+++ Projet/src/map/Phare.java
@@ -8,6 +8,9 @@ import org.newdawn.slick.SpriteSheet;
 import game.Game;
 import joueur.Joueur;
 
+/**
+ * Case spécifique représentant un Phare (cf. classe Case pour plus d'informations)
+ */
 public class Phare extends Case {
 	public static final int ID = 2;
 	private static final int NB_FRAMES_ANIMATION = 6;
@@ -16,6 +19,10 @@ public class Phare extends Case {
 	private Animation animPharesJoueurs[] = new Animation[Game.NB_JOUEURS];
 	private int joueurPossesseur = -1;//le phare appartient a personne par defaut
 	
+	/**
+	 * Créée une case Phare.
+	 * @param position La position de la case (dans la fenêtre) relative à l'origine de la map.
+	 */
 	public Phare(Point position) {
 		super(ID, NB_FRAMES_ANIMATION, DUREE_FRAME, position);
 		for (int j = 0; j < animPharesJoueurs.length; j++) {
@@ -27,10 +34,18 @@ public class Phare extends Case {
 		this.start();
 	}
 	
+	/**
+	 * Renvoie l'id du joueur possesseur du phare ou -1 si aucun joueur ne possède le phare.
+	 * @return L'id du joueur possesseur du phare ou -1 si aucun joueur ne possède le phare.
+	 */
 	public int getJoueurPossesseur() {
 		return joueurPossesseur;
 	}
 	
+	/**
+	 * Asigne le phare à un joueur (ou le rend neutre)
+	 * @param joueur L'id du joueur à qui le phare est asigné (ou -1 pour rendre le phare neutre)
+	 */
 	public void setJoueurPossesseur(int joueur) {
 		if (joueur >= 0 && joueur < Game.NB_JOUEURS) {
 			joueurPossesseur = joueur;
@@ -39,10 +54,17 @@ public class Phare extends Case {
 		}
 	}
 	
+	/**
+	 * Rend le phare neutre
+	 */
 	public void rendrePhareNeutre() {
 		joueurPossesseur = -1;
 	}
 	
+	/**
+	 * Dessine le phare sur la fenêtre, dans le référentiel de la map, en prenant en compte l'échelle ; 
+	 * le phare prend la couleur blanche, rouge ou bleu selon qu'il soit neutre, pris par le joueur 1 ou pris par le joueur 2
+	 */
 	@Override
 	public void draw() {
 		if (joueurPossesseur == -1) {
